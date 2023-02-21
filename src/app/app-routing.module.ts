7c80f034be2cb7f64159d33e0ea89a32aefda850
Laura Perez <laurap48@hotmail.com>
2023-02-21 16:16:23 +0100

    Creación de componentes, rutas e instalación de Bootstrap
    Creación de los componentes, las rutas e instalación de Bootstrap en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoUsuariosComponent } from './components/listado-usuarios/listado-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: ListadoUsuariosComponent},
+  {path: 'Home', component: ListadoUsuariosComponent},
+  {path: 'Usuario', component: UsuarioComponent},
+  {path: 'Nuevo Usuario', component: FormularioComponent},
+  {path: '**', component: ListadoUsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListadoUsuariosComponent } from './components/listado-usuarios/listado-usuarios.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListadoUsuariosComponent,
+    UsuarioComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,

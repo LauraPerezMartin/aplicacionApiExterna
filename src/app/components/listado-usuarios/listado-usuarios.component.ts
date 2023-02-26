f12e18cc52d2008f8859c636863985109b5bf5a2
Laura Perez <laurap48@hotmail.com>
2023-02-26 12:29:37 +0100

    Vista y carga de listado de usuario
    Vista del listado de usuarios que es el home y carga de listado

--- src/app/components/listado-usuarios/listado-usuarios.component.ts
+++ src/app/components/listado-usuarios/listado-usuarios.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/usuario.interface';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-listado-usuarios',
   templateUrl: './listado-usuarios.component.html',
   styleUrls: ['./listado-usuarios.component.css']
 })
-export class ListadoUsuariosComponent {
+export class ListadoUsuariosComponent implements OnInit {
+  arrUsers: Usuario[] | any;
+  pag: number = 1;
 
+  constructor(private usuariosService: UsuariosService) { }
+
+  ngOnInit(): void {
+    this.arrUsers = this.usuariosService.getAllUsers();
+  }
+
+  borrarUsuario(): void {
+    alert('Usuario borrado');
+  }
 }

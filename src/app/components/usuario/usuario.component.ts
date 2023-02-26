010cdf6d684743427d6007cbac6a0864e3d855c9
Laura Perez <laurap48@hotmail.com>
2023-02-26 17:30:24 +0100

    Vista y carga de usuario
    Vista detalle de cada usuario

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario.interface';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit {
+
+  usuario: Usuario | any;
+
+  constructor (
+    private usuariosService: UsuariosService, 
+    private activatedRoute: ActivatedRoute
+    ) {}
+
+  ngOnInit (): void {
+    this.activatedRoute.params.subscribe((params: any)=>{
+      let iduser: number = parseInt(params.iduser);
+      //let idnumber: number = parseInt(iduser);
+      this.usuario = this.usuariosService.getUserById(iduser);
+    })
+  }
+
+  borrarUsuario() : any {
+    alert('Borrar usuario');
+  }
 
 }

3c8ee0df94001b6addbd374a89fc751777c33b0b
Laura Perez <laurap48@hotmail.com>
2023-03-04 21:17:44 +0100

    Conectar formulario con la API

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/interfaces/usuario.interface';
 import { UsuariosService } from 'src/app/services/usuarios.service';
@@ -14,13 +14,15 @@ export class FormularioComponent implements OnInit {
 
   formUsuario: FormGroup;
   usuario: Usuario | any;
-  accion: string = 'Guardar';
+  accion: string = 'Crear';
+  idUser: string | any;
 
   constructor(
     private usuariosService: UsuariosService,
-    private activatedRoute: ActivatedRoute
-
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
+
     this.formUsuario = new FormGroup({
       first_name: new FormControl('', [
         Validators.required,
@@ -33,82 +35,90 @@ export class FormularioComponent implements OnInit {
       username: new FormControl('', []),
       email: new FormControl('', [
         Validators.required,
-        Validators.pattern(/^\w+@[a-zA-Z_.]+?\.[a-zA-Z]{2,5}$/),
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/)
       ]),
       image: new FormControl('', [
         Validators.required,
-        //Validators.minLength(5)
-        Validators.pattern(/^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,5}$/)
+        Validators.pattern(/^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/)
       ]),
     })
   }
+
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: any) => {
-      console.log(this.accion);
-      if (params.iduser !== undefined) {
-        console.log(params.iduser);
-        this.accion = 'Modificar';
-        let iduser: number = parseInt(params.iduser);
-        this.usuario = this.usuariosService.getUserById(iduser);
-        this.formUsuario = new FormGroup({
-          first_name: new FormControl(this.usuario.first_name, [
-            Validators.required,
-            Validators.minLength(3)
-          ]),
-          last_name: new FormControl(this.usuario.last_name, [
-            Validators.required,
-            Validators.minLength(3)
-          ]),
-          username: new FormControl('', []),
-          email: new FormControl(this.usuario.email, [
-            Validators.required,
-            Validators.pattern(/^\w+@[a-zA-Z_0-9\.]+?\.[a-zA-Z]{2,6}$/),
-          ]),
-          image: new FormControl(this.usuario.image, [
-            Validators.pattern(/^(http|https):\/\/[a-zA-Z0-9\-\.\?@\/]$/) // comprobaros formato url http://dominio.com
-          ]),
-        })
+    //si la url trae la id del usuario traemos los datos del usuario y los cargamos en el formulario
+    this.activatedRoute.params.subscribe(async (params: any): Promise<void> => {
+      this.idUser = params.iduser;
+      if (this.idUser) {
+        try {
+          this.accion = 'Modificar';
+          this.usuario = await this.usuariosService.getUserById(this.idUser);
+          this.formUsuario = new FormGroup({
+            _id: new FormControl(this.idUser, []),
+            first_name: new FormControl(this.usuario.first_name, [
+              Validators.required,
+              Validators.minLength(3)
+            ]),
+            last_name: new FormControl(this.usuario.last_name, [
+              Validators.required,
+              Validators.minLength(3)
+            ]),
+            username: new FormControl('', []),
+            email: new FormControl(this.usuario.email, [
+              Validators.required,
+              Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/),
+            ]),
+            image: new FormControl(this.usuario.image, [
+              Validators.required,
+              Validators.pattern(/^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/) // comprobamos formato url http://dominio.com
+            ]),
+          })
+        } catch (error) {
+          Swal.fire('Error', 'Error al conectarse con la base de datos', 'error')
+        }
       }
     })
   }
 
-  dataNuevoUsuario() {
-    if (this.accion === 'Guardar') {
-      let nuevoUsuario: Usuario = this.formUsuario.value;
-      let guardado: boolean = this.usuariosService.postUser(nuevoUsuario);
-      if (guardado) {
-        Swal.fire(
-          'Usuario Creado',
-          'El usuario se ha creado correctamente',
-          'success'
-        )
-        alert('El usuario se ha creado correctamente')
-      } else {
-        Swal.fire(
-          'Error',
-          'El usuario no se ha creado, intentalo de nuevo',
-          'error'
-        )
+  //como no capturamos el username en el formulario, lo creamos tal y como esta en la API con de los datos del usuario
+  obtenerUsername(nombre: string, apellido: string): string {
+    let arrNombre: string[] = nombre.split(' ');
+    let arrApellido: string[] = apellido.split(' ');
+    let username: string = `${arrNombre.join('')}.${arrApellido[0]}`;
+    return username;
+  }
 
+  async dataUsuario(): Promise<void> {
+    let datosUsuario: Usuario = this.formUsuario.value;
+    datosUsuario.username = this.obtenerUsername(datosUsuario.first_name, datosUsuario.last_name); //por si al modificar el formulario se cambia el nombre y el apellido lo creamos siempre
+    if (!this.idUser) {
+      try {
+        let response: Usuario = await this.usuariosService.postUser(datosUsuario);
+        if (response.id) {
+          Swal.fire('Usuario Creado', 'El usuario se ha creado correctamente', 'success')
+            .then((result) => {
+              this.router.navigate(['/home'])
+            })
+        } else {
+          Swal.fire('Error', 'El usuario no se ha creado, intentalo de nuevo', 'error')
+        }
+      } catch (error) {
+        Swal.fire('Error', 'Error al conectarse con la base de datos', 'error')
       }
     } else {
-      let datosUsuario: Usuario = this.formUsuario.value;
-      let guardado: boolean = this.usuariosService.putUser(datosUsuario);
-      if (guardado) {
-        Swal.fire(
-          'Guardado',
-          'El usuario se ha modificado correctamente',
-          'success'
-        )
-      } else {
-        Swal.fire(
-          'Error',
-          'El usuario no se ha modificado, intentalo de nuevo',
-          'error'
-        )
+      try {
+        let response = await this.usuariosService.putUser(datosUsuario);
+        if (response._id) {
+          Swal.fire('Guardado', 'El usuario se ha modificado correctamente', 'success')
+            .then((result) => {
+              this.router.navigate(['/home'])
+            })
+        } else {
+          Swal.fire('Error', 'El usuario que intentas editar no existe', 'error')
+        }
+      } catch (error) {
+        Swal.fire('Error', 'Error al conectarse con la base de datos', 'error')
       }
     }
-
     this.formUsuario.reset()
   }
 
@@ -118,5 +128,4 @@ export class FormularioComponent implements OnInit {
     }
     return false;
   }
-
 }
